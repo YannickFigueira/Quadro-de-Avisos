808861ba1ee846414e89583add869f66428ee7c0
YannickFigueira <chronostimeinchain@gmail.com>
2026-05-17 22:03:56 -0300

    Desenhado janela

---
+++ avisos.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import argparse
+
+VERSION='1.0.0'
+class Avisos:
+    # Principal
+    def __init__(self, root):
+
+        # Versão para deb
+        parser = argparse.ArgumentParser(prog="avisos")
+        parser.add_argument("--version", action="version", version=f"%(prog)s {VERSION}")
+
+        # Janela
+        self.root = root
+        self.root.title('Anúncios')
+        self.root.resizable(False, False)
+
+        # Controle
+        linha = 0
+
+        self.selecao_lbl = tk.Label(self.root, text='Selecione o arquivo para anúncio')
+        self.selecao_lbl.grid(row=linha, column=1, padx=5, pady=5)
+        linha += 1
+
+        self.carregar_arquivo_btn=tk.Button(self.root, text='Carregar Arquivo',)
+        self.carregar_arquivo_btn.grid(row=linha, column=1, padx=5, pady=5)
+
+
+if __name__ == '__main__':
+    avisos = tk.Tk()
+    app = Avisos(avisos)
+    avisos.mainloop()
